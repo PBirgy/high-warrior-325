0f3c6fcb795bfe73bf38ca643339eb9ccd79801a
unknown <Paul@.(none)>
2011-03-24 00:09:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HighWarrior325::Application.config.secret_token = '43b0c0dea1ddfb5b1540ff056c3926fb40b96b14dabf3a97615772168e1e91a3ec4d217d3a8581885691e7bd3fcda3b89517228121b35fc24ba03e084e6bff43'
